a3429ddc4e367d27b7c7c1feaa7f22d6aae4a2ec
Dian <dian@petanikode.com>
2024-04-05 04:58:04 +0800

    fix: middleware

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -3,13 +3,7 @@ const jwt = require('jsonwebtoken');
 function authToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    if (!token) {
-        console.log(token);
-        return res.status(401).json({
-            message: "Invalid token"
-        });
-    }
-  
+      
     jwt.verify(token, 'RAHASIA_NEGARA', (err, user) => {
         if (err) {
             return res.status(403).json({
