9bad639b4d81e18771163b6a9c6eb8e6d25a9381
Dian <dian@petanikode.com>
2024-04-06 05:27:22 +0800

    fix upload

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -4,21 +4,26 @@ var router = express.Router();
 const path = require('path');
 const multer = require('multer');
 const { ulid } = require('ulid');
+const fs = require("node:fs");
 
 const { Product } = require('../../db/model');
 
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public/images')
+        cb(null, 'public/images/uploads')
     },
     filename: function (req, file, cb) {
         fileExtension = path.extname(file.originalname);
-        cb(null, ulid() + "." + fileExtension)
+        cb(null, ulid() + fileExtension)
     }
 })
 
 const upload = multer({ storage: storage })
+
+function removeUploadedFile(filename) {
+    return fs.unlinkSync("public/images/uploads/" + filename);
+}
 
 
 /* GET product listing. */
@@ -44,13 +49,30 @@ router.post('/', upload.single('image'), async function (req, res) {
     });
 })
 
-router.put('/:id', async (req, res) => {
-    const product = await Product.findByPk(req.params.id);
-    if (product) {
-        const updatedProduct = await Product.update(req.body, {
+router.put('/:id', upload.single('image'), async (req, res) => {
+    const oldProduct = await Product.findByPk(req.params.id);
+    if (oldProduct) {
+        let newProduct = req.body;
+
+        if (req.file) {
+            try {
+                // remove old uploaded file
+                removeUploadedFile(oldProduct.image);
+
+                const image = req.file.filename ?? "https://placehold.co/600x400?text=no%20image";
+                newProduct = { ...newProduct, image }
+            } catch (error) {
+
+            }
+        }
+
+        const updatedProduct = await Product.update(newProduct, {
             where: { id: req.params.id }
         });
-        res.json(updatedProduct);
+        res.json({
+            message: "Produk berhasil diupdate",
+            data: updatedProduct
+        });
     } else {
         res.status(404).json({ message: 'Product not found' });
     }
@@ -59,7 +81,15 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     const product = await Product.findByPk(req.params.id);
     if (product) {
+        // hapus data dari db
         Product.destroy({ where: { id: req.params.id } });
+        try {            
+            // hapus image
+            removeUploadedFile(product.image);
+        } catch (error) {
+            
+        }
+
         res.json({ message: "Produk berhasil dihapus" });
     } else {
         res.status(404).json({ message: "Product not found" });
