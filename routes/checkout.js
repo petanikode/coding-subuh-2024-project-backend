20a264e1b2ac14ea91d638415d2776b146b50532
Ahmad Muhardian <dian@petanikode.com>
2024-04-14 10:01:03 +0000

    add xendit

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,93 +1,130 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const sequelize = require('sequelize');
 const { Cart, Transaction } = require("../db/model");
 
-// NOTE: seharusnya nggak di hardcode di sini
-const XENDIT_SECRET_API_KEY = "xnd_development_EqauLIMFflfTPXB1w58Ca79xr2PFI4IFYW1ak1dtxIBkVtjFl3t9JJzYbpSTfG";
+const XENDIT_SECRET_API_KEY = process.env["XENDIT_SECRET_API_KEY"];
 
-const { Xendit } = require('xendit-node');
+const { Xendit } = require("xendit-node");
+const authToken = require("../middleware/authToken");
 
 const xenditClient = new Xendit({
-    secretKey: XENDIT_SECRET_API_KEY,
-})
+  secretKey: XENDIT_SECRET_API_KEY,
+});
 
-const { Invoice } = xenditClient
+const { Invoice } = xenditClient;
 
-router.post('/', async (req, res) => {
-    const userId = req.user.id;
-    const { items } = req.body;
+router.post("/", authToken, async (req, res) => {
+  const userId = req.user.id;
+  const { items } = req.body;
 
-    console.log("ITEMS TERKIRIM: ", items)
+  console.log("ITEMS TERKIRIM: ", items);
 
-    try {
+  try {
+    let total = 0;
+    items.forEach(
+      (i) => (total += Number(i.product.price) * Number(i.quantity)),
+    );
 
-        let total = 0;
-        items.forEach(i => total += Number(i.product.price) * Number(i.quantity));
+    console.log("TOTAL: ", total);
 
-        console.log("TOTAL: ", total)
+    // simpan data tranasaksi ke database
+    const transactionCreated = await Transaction.create({
+      userId,
+      details: items,
+      total,
+    });
 
-        const transactionCreated = await Transaction.create({
-            userId,
-            details: items,
-            total
-        });
+    // buat invoice dengan payment gateway
+    const xenditResponse = await Invoice.createInvoice({
+      data: {
+        amount: total,
+        invoiceDuration: 172800,
+        externalId: transactionCreated.id,
+        description: "Pembayaran produk di Aplikasi Toko",
+        currency: "IDR",
+        reminderTime: 1,
+        items: items.map((i) => {
+          return {
+            name: i.product.name,
+            price: Number(i.product.price),
+            quantity: Number(i.quantity),
+          };
+        }),
+      },
+    });
 
-        // buat invoice dengan payment gateway
-        const xenditResponse = await Invoice.createInvoice({
-            data: {
-                amount: total,
-                invoiceDuration: 172800,
-                externalId: transactionCreated.id,
-                description: "Pembayaran produk di Aplikasi Toko",
-                currency: "IDR",
-                reminderTime: 1,
-                items: items.map(i => {
-                    return {
-                        name: i.product.name,
-                        price: Number(i.product.price),
-                        quantity: Number(i.quantity)
-                    }
+    console.log("XENDIT_______", xenditResponse);
 
-                })
-            }
-        });
+    if (xenditResponse) {
+      // simpan invoice ke tabel transaski
+      transactionCreated.invoiceId = xenditResponse.id;
+      transactionCreated.invoiceUrl = xenditResponse.invoiceUrl;
+      await transactionCreated.save();
 
-        console.log("XENDIT_______", xenditResponse);
+      // hapus semua item di keranjang user
+      await Cart.destroy({
+        where: {
+          userId,
+        },
+      });
 
-        if (xenditResponse) {
-            // simpan invoice ke tabel transaski
-            transactionCreated.invoiceId = xenditResponse.id;
-            transactionCreated.invoiceUrl = xenditResponse.invoiceUrl;
-            await transactionCreated.save();
-
-            // hapus semua item di keranjang user
-            await Cart.destroy({
-                where: {
-                    userId
-                }
-            })
-
-
-            res.json({
-                message: "Tranasaksi berhasi dibuat",
-                invoiceUrl: xenditResponse.invoiceUrl,
-                data: transactionCreated
-            })
-        }
-
-        
-    } catch (error) {
-        res.status(400).json({
-            message: error.message
-        })
+      res.json({
+        message: "Tranasaksi berhasi dibuat",
+        invoiceUrl: xenditResponse.invoiceUrl,
+        data: transactionCreated,
+      });
     }
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 });
 
 // /payment/callback
-router.post('/callback', async (req, res) => {
-    // TODO: update stock produk setelah sukses bayar
-    console.log(req.body);
+router.post("/callback", async (req, res) => {
+  console.log(req.body);
+  try {
+    const XENDIT_CALLBACK_TOKEN = process.env["XENDIT_CALLBACK_TOKEN"];
+
+    if (req.headers["x-callback-token"] !== XENDIT_CALLBACK_TOKEN) {
+      return res.status(401).json({
+        message: "invalid token",
+      });
+    }
+
+    const trx = await Transaction.findOne({
+      where: {
+        invoiceId: req.body.id
+      }
+    });
+
+    if (!trx) {
+      return res.status(404).json({
+        message: "Tranasaction not found",
+      });
+    }
+
+    if (req.body.status === "PAID") {
+      trx.status = "PAID";
+      trx.save();
+
+      return res.json({
+        message: "Pembayaran berhasil",
+      });
+      // TODO: update stock produk setelah sukses bayar
+    } else {
+      trx.status = "CANCELED";
+      trx.save();
+
+      return res.json({
+        message: "Pembayaran gagal",
+      });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
